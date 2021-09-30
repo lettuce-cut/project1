d452dfdc53331d4126f201c1e7d35b9c4292ce61
Sailor Ong <valleywind15@gmail.com>
2021-09-30 01:07:45 -0600

    state

--- ID.cpp
+++ ID.cpp
@@ -4,8 +4,9 @@ void IDAutomaton::S0(const std::string& input) {
     if (std::isalpha(input[index])) {
         while (std::isalnum(input[index])) {
             inputRead++;
+            index++;
+            S0(input);
         }
-//        inputRead= inputRead-1;
     }
     else {
         Serr();
