668aa14d70a98925665d865422fb4b4634eb92b9
Sailor Ong <valleywind15@gmail.com>
2021-09-30 00:01:45 -0600

    plz

--- ID.cpp
+++ ID.cpp
@@ -1,7 +1,7 @@
 #include "ID.h"
 #include <iostream>
 void IDAutomaton::S0(const std::string& input) {
-    if (std::isalpha(input[index]) == true) {
+    if (std::isalpha(input[index])) {
         while (index < input.size()) {
             inputRead++;
             index++;
